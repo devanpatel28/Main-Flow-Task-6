88f6b3a7a4e5551bb6d150e5f404e61cfd73441a
devan <devanbhensdadiya123@gmail.com>
2024-11-22 10:17:00 +0530

    first commit

---
+++ login-signup-backend/controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Error registering user' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Error logging in' });
+  }
+};
